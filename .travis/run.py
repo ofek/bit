50337ba37c23665335f3006dcdb0f3904077a79a
Ofekmeister <ofekmeister@gmail.com>
2016-11-12 17:26:07 -0500

    try tox-travis again

--- .travis/run.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-version = os.environ.get('TRAVIS_PYTHON_VERSION')
-if version and version.startswith('pypy3.3-5.2-'):
-    subprocess.call(['pip', 'install', '-U', 'virtualenv'])
-
-subprocess.call(['source', '~/.venv/bin/activate'], shell=True)
-subprocess.call(['tox', '--', '-rs'])
