520a8468415f9314e70c780d457aa4f129d673ff
Ofekmeister <ofekmeister@gmail.com>
2016-11-12 17:19:13 -0500

    ...

--- .travis/run.py
+++ .travis/run.py
@@ -5,4 +5,5 @@ version = os.environ.get('TRAVIS_PYTHON_VERSION')
 if version and version.startswith('pypy3.3-5.2-'):
     subprocess.call(['pip', 'install', '-U', 'virtualenv'])
 
+subprocess.call(['source', '~/.venv/bin/activate'])
 subprocess.call(['tox', '--', '-rs'])
