2f8c236730f6e8180ddd1b4a350a7d500f1be731
ofek <ofekmeister@gmail.com>
2016-11-14 19:55:17 -0500

    add flip_hex_byte_order

--- bit/utils.py
+++ bit/utils.py
@@ -1,23 +1,27 @@
 from binascii import hexlify
 
 
-def int_to_unknown_bytes(num, endian='big'):
+def int_to_unknown_bytes(num, byteorder='big'):
     """Converts an int to the least number of bytes as possible."""
 
-    if endian not in ('big', 'little'):
-        raise ValueError('{} is an unrecognized endianness.'.format(endian))
+    if byteorder not in ('big', 'little'):
+        raise ValueError('{} is an unrecognized endianness.'.format(byteorder))
 
     # Round up bit length required to represent number as it is
     # unknown at this point. Doing (bit_length // 8) + 1 is
     # incorrect when length is a multiple of 8.
-    return num.to_bytes((num.bit_length() + 7) // 8, endian)
+    return num.to_bytes((num.bit_length() + 7) // 8, byteorder)
 
 
 def bytes_to_hex(bytestr, upper=True):
-    hexlified = hexlify(bytestr).decode()
-    return hexlified.upper() if upper else hexlified
+    hexed = hexlify(bytestr).decode()
+    return hexed.upper() if upper else hexed
 
 
 def int_to_hex(num, upper=True):
-    hexlified = hex(num)[2:]
-    return hexlified.upper() if upper else hexlified
+    hexed = hex(num)[2:]
+    return hexed.upper() if upper else hexed
+
+
+def flip_hex_byte_order(string):
+    return bytes_to_hex(bytes.fromhex(string)[::-1])
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from bit.utils import bytes_to_hex, int_to_hex, int_to_unknown_bytes
+from bit.utils import bytes_to_hex, flip_hex_byte_order, int_to_hex, int_to_unknown_bytes
 
 BIG_INT = 123456789 ** 5
 BYTES_BIG = b'TH8\xe2\xaaN\xd7^aX7\x93\xe7\xc6\xa3\x02\x85'
@@ -36,11 +36,5 @@ class TestIntToHex:
         assert int_to_hex(BIG_INT, upper=False) == HEX.lower()
 
 
-
-
-
-
-
-
-
-
+def test_flip_hex_byte_order():
+    assert flip_hex_byte_order(bytes_to_hex(BYTES_LITTLE)) == HEX
