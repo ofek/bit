6d9fb3bd843e380efcbabd08ec55af0c0a06c792
ofek <ofekmeister@gmail.com>
2017-04-10 23:22:01 -0400

    drop python 3.3 & 3.4

--- setup.py
+++ setup.py
@@ -34,8 +34,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
