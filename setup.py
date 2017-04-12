de50cdef9878ce3da96b0448490fde9bade8f0a3
ofek <ofekmeister@gmail.com>
2017-04-11 22:29:04 -0400

    fix deps

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy'
     ),
 
-    install_requires=('coincurve>=4.1.0', 'cryptography>=1.8.1'),
+    install_requires=('coincurve>=4.1.0', 'requests'),
     extras_require={
         'cli': ('appdirs', 'click', 'privy', 'tinydb'),
         'cache': ('lmdb', ),
