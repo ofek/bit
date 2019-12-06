d2ba17c76a84e4a62db08e35af7dbdfd9835427b
manfromsiberia <manfromsiberia@yandex.ru>
2019-12-06 15:25:16 +0300

    add correct handling for param fee in prepare_transaction when fee is 0 +sanity check for fee type, should fix #90

--- bit/wallet.py
+++ bit/wallet.py
@@ -766,10 +766,12 @@ class PrivateKeyTestnet(BaseKey):
         :returns: JSON storing data required to create an offline transaction.
         :rtype: ``str``
         """
+        if not isinstance(fee, (int, type(None))):
+            raise TypeError('Invalid fee type.')
         unspents, outputs = sanitize_tx_data(
             unspents or NetworkAPI.get_unspent_testnet(address),
             outputs,
-            fee or get_fee_cached(),
+            fee if isinstance(fee, int) else get_fee_cached(),
             leftover or address,
             combine=combine,
             message=message,
